291f32736056bdb74caf3efcfabe1ee466a8d8f6
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-10-16 14:57:37 -0500

    GET request data from DynamoDB AWS table

--- drinks/views.py
+++ drinks/views.py
@@ -1,7 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+import boto3
 
 @api_view(["GET"])
 def home(request):
-    return Response({"test": "data"})
+    db = boto3.resource("dynamodb")
+    table = db.Table("drinks")
+    drinks = table.scan() # get all of the table and store in variable drinks
+    # return Response(drinks) # this will show table info and more useless info of the table and dynamoDB AWS
+    return Response({"drinks": drinks.get("Items")}) # this will only show the "Items" 
